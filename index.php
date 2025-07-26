e2c7810f13e6dd547caf74ea608451d9b2405b67
Penfold-88 <paul.ludditt@live.co.uk>
2025-07-26 19:42:01 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include 'header.php'; ?>
-<?php include 'nav.php'; ?>
-
-<main style="text-align: center; padding: 50px;">
-    <h2>Welcome to the DCS Player Statistics Dashboard</h2>
-    <p>Select a section from the navigation menu to get started.</p>
-</main>
-
-<?php include 'footer.php'; ?>
