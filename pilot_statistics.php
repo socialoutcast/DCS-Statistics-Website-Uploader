f2f02b98fa16913b5e0ec330365bbc864101112f
Penfold-88 <paul.ludditt@live.co.uk>
2025-07-26 19:42:39 +0100

    Delete pilot_statistics.php

--- pilot_statistics.php
+++
@@ -1,7 +0,0 @@
-<?php include 'header.php'; ?>
-<?php include 'nav.php'; ?>
-<main>
-    <h2>Pilot Statistics</h2>
-    <p>This is the Pilot Statistics page.</p>
-</main>
-<?php include 'footer.php'; ?>
